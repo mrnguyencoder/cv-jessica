cf937468b1d8bb99d8fe217399ed4da9c47cec22
mrnguyencoder <mr.nguyencoder@gmail.com>
2023-02-09 14:20:16 +0100

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Footer from "./components/Footer"
+import Main from "./components/Main"
+import NavBar from "./components/NavBar"
 
 
 function App() {
   return (
-    <div className="">
-    <p className="">test</p>
+    <div className="bg-slate-50 text-slate-900">
+      <NavBar/>
+      <Main/>
+      <Footer/>
     </div>
   )
 }
